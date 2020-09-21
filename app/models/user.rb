7bf580abf006f1b72b50491189dd110aae8e0ed4
WilsonLi <ligokdoon@gmail.com>
2020-09-21 13:40:08 -0400

    user model created

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    validates :first_name, :last_name, :password_digest, presence: true 
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+    
+
+    def self.generate_session_token 
+        SecureRandom::urlsafe_base64()
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by_username(username)
+        if user && user.is_password?(password)
+            return user
+        else 
+            return nil 
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = Bcrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+end
