8bcec88b0554af179f8894540e6636c4f71b4aad
WilsonLi <ligokdoon@gmail.com>
2020-09-21 18:12:51 -0400

    initial Auth branch commit

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>SURO TEST FROM APP COMPONENT</h1>
+        </div>
+    )
+}
+
+export default App;
--- frontend/suro.jsx
+++ frontend/suro.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-// import Root from './components/root';
-import { logIn, logOut } from './util/session_api_util';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const root = document.getElementById("root");
     const store = configureStore();
 
-    window.login = logIn;
-    window.logout = logOut;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     
-    ReactDOM.render(<h1>SURO TEST</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 })
