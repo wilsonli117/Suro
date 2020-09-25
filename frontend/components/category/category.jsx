c4337b00b49c47b33d7539ca3658b38cc9449b3e
WilsonLi <ligokdoon@gmail.com>
2020-09-25 00:59:47 -0400

    fixed bugs in category mock component

--- frontend/components/category/category.jsx
+++ frontend/components/category/category.jsx
@@ -6,14 +6,14 @@ const Category = props => {
             <h2 className="category-browse">Browse by category</h2>
             <div className="category">
                 <ul class="categories">
-                    <div><i class="fas fa-angle-left fa-2x"></i></div>
+                    <div><i className="fas fa-angle-left fa-2x"></i></div>
                     <li><img src={window.carURL} /><div>Car</div></li>
                     <li><img src={window.classicURL} /><div>Classics</div></li>
                     <li><img src={window.convertibleURL} /><div>Convertibles</div></li>
                     <li><img src={window.exoticURL} /><div>Exotic & luxury</div></li>
                     <li><img src={window.suvURL} /><div>SUVs</div></li>
                     <li><img src={window.sportURL} /><div>Sport</div></li>
-                    <div><i class="fas fa-angle-right fa-2x"></i></div>
+                    <div><i className="fas fa-angle-right fa-2x"></i></div>
                 </ul>
             </div>
         </>
