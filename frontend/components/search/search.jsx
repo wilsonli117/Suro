20b7ac41b7acce9f7917a8c52e2f8fffdb34c379
WilsonLi <ligokdoon@gmail.com>
2020-09-24 22:32:40 -0400

    splash page progress

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -47,10 +47,37 @@ const Search = props => {
                     </div>
 
                 </div>
-                <div className="div-city"></div>
+              
             </div>
-            <div>
+            <div className="covid">
+                <div className="div-city"></div>
+                <div className="insurance">
+                    <p>INSURANCE PROVIDER</p>
+                    <button><img src="/assets/insurance.png" alt="Nationwide is on your side" /></button>
+                </div>
 
+                <p>Hit the road confidently</p>
+                <div className="safe-sharing"><h2>Safe car sharing while navigating COVID-19</h2></div>
+                <ul>
+                    <li>
+                        <img src="/assets/cleaning.png"/>
+                        <div>Clean & disinfected cars</div>
+                        <p>The enhanced cleaning policy requires hosts to clean & disinfect their cars so you can feel good behind the wheel.</p>
+                        <button className="learn-button">Learn more</button>
+                    </li>
+                    <li>
+                        <img src="/assets/hearthand"/>
+                        <div>Contactless check-in options</div>
+                        <p>Maintain physical distance during check-in and checkout with a variety of remote access options.</p>
+                        <button className="learn-button">Learn more</button>
+                    </li>
+                    <li>
+                        <img src="/assets/thumbsup"/>
+                        <div>Flexible cancellations</div>
+                        <p>Cancel for free up to 24 hours before your trip starts. Plans can change and it helps to be flexible when they do.</p>
+                        <button className="learn-button">Learn more</button>
+                    </li>
+                </ul>
             </div>
         </>
     )
