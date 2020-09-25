f9b848d77cfc3186dc54f37462bcf510873be75a
WilsonLi <ligokdoon@gmail.com>
2020-09-25 00:48:28 -0400

    added mock category component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { AuthRoute } from '../util/route_util';
 import Modal from './modal/modal';
 import NavBar from './navbar_container';
 import Search from './search/search';
+import Category from './category/category';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
@@ -14,7 +15,7 @@ const App = () => {
                 <NavBar />
             </header>
             <Search />
-        
+            <Category />
         </div>
     )
 }
--- frontend/components/category/category.jsx
+++ frontend/components/category/category.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 const Category = props => {
     return (
-        <div className="category">
-            <h2>Browse by category</h2>
-            <div><i class="fas fa-angle-right"></i></div>
-            <ul class="categories">
-                <li><img src=""/></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-            <div><i class="fas fa-angle-left"></i></div>
-        </div>
+        <>
+            <h2 className="category-browse">Browse by category</h2>
+            <div className="category">
+                <ul class="categories">
+                    <div><i class="fas fa-angle-left fa-2x"></i></div>
+                    <li><img src={window.carURL} /><div>Car</div></li>
+                    <li><img src={window.classicURL} /><div>Classics</div></li>
+                    <li><img src={window.convertibleURL} /><div>Convertibles</div></li>
+                    <li><img src={window.exoticURL} /><div>Exotic & luxury</div></li>
+                    <li><img src={window.suvURL} /><div>SUVs</div></li>
+                    <li><img src={window.sportURL} /><div>Sport</div></li>
+                    <div><i class="fas fa-angle-right fa-2x"></i></div>
+                </ul>
+            </div>
+        </>
     )
 }
+
+export default Category;
