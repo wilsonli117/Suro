18b483ada498c2c84c509d34d6b8fe87fd512822
WilsonLi <ligokdoon@gmail.com>
2020-09-24 22:54:37 -0400

    3rd heroku push

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,7 +4,7 @@ const Search = props => {
     return (
         <>
             <div className='search'>
-                <img className="slashes" src="/assets/slashes.png"/>
+                <img className="slashes" src={window.slashesURL}/>
         
                     <div className="search-div">
                     <h1>Way better than a rental car</h1>
@@ -53,26 +53,26 @@ const Search = props => {
                 <div className="div-city"></div>
                 <div className="insurance">
                     <p>INSURANCE PROVIDER</p>
-                    <button><img src="/assets/insurance.png" alt="Nationwide is on your side" /></button>
+                    <button><img src={window.insuranceURL} alt="Nationwide is on your side" /></button>
                 </div>
 
                 <p>Hit the road confidently</p>
                 <div className="safe-sharing"><h2>Safe car sharing while navigating COVID-19</h2></div>
                 <ul>
                     <li>
-                        <img src="/assets/cleaning.png"/>
+                        <img src={window.cleaningURL}/>
                         <div>Clean & disinfected cars</div>
                         <p>The enhanced cleaning policy requires hosts to clean & disinfect their cars so you can feel good behind the wheel.</p>
                         <button className="learn-button">Learn more</button>
                     </li>
                     <li>
-                        <img src="/assets/hearthand"/>
+                        <img src={window.hearthandURL}/>
                         <div>Contactless check-in options</div>
                         <p>Maintain physical distance during check-in and checkout with a variety of remote access options.</p>
                         <button className="learn-button">Learn more</button>
                     </li>
                     <li>
-                        <img src="/assets/thumbsup"/>
+                        <img src={window.thumbsupURL}/>
                         <div>Flexible cancellations</div>
                         <p>Cancel for free up to 24 hours before your trip starts. Plans can change and it helps to be flexible when they do.</p>
                         <button className="learn-button">Learn more</button>
