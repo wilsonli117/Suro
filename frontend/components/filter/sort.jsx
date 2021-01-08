dcc0a899924785837d301bc60a77727867a8821d
WilsonLi <ligokdoon@gmail.com>
2021-01-07 21:21:40 -0500

    add dispatch and selector hooks to sort component

--- frontend/components/filter/sort.jsx
+++ frontend/components/filter/sort.jsx
@@ -1,28 +1,34 @@
 import React, { useState} from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateFilter } from '../../actions/filter_actions'
 
 const Sort = props => {
 
+    const currentFilter = useSelector((state) => state.ui.filters.sort);
     const [sort, setSort] = useState('relevance');
     const dispatch = useDispatch();
 
     const handleSubmit = e => {
+        debugger;
         e.preventDefault();
-        setSort(e.currentTarget.value);
+        dispatch(updateFilter('sort', sort));
+        
     }
+
+    debugger;
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <input type="radio" name="sortType" id="sortType" value ="relevance" onChange={ e => setSort(e.target.value)}/>
+                <input type="radio" name="sortType" id="sortType" value ="relevance" checked={currentFilter === 'relevance' ? "checked" : null} onChange={ e => setSort(e.target.value)}/>
                 <label htmlFor="relevance">Relevance</label>
-                <input type="radio" name="sortType" id="sortType" value="lowToHigh" onChange={e => setSort(e.target.value)}/>
+                <input type="radio" name="sortType" id="sortType" value="lowToHigh" checked={currentFilter === 'lowToHigh' ? "checked" : null} onChange={e => setSort(e.target.value)}/>
                 <label htmlFor="lowToHigh">Price: low to high</label>
-                <input type="radio" name="sortType" id="sortType" value="highToLow" onChange={e => setSort(e.target.value)}/>
+                <input type="radio" name="sortType" id="sortType" value="highToLow" checked={currentFilter === 'highToLow' ? "checked" : null} onChange={e => setSort(e.target.value)}/>
                 <label htmlFor="highToLow">Price: high to low</label>
                 <button type="submit">Apply</button>
             </form>
-            <i class="fas fa-times" onClick={() => dispatch(props.closeModal())}></i>
+            <i className="fas fa-times" onClick={() => dispatch(props.closeModal())}></i>
         </>
     )
 }
